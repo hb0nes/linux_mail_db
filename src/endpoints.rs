414c88e16d48139004770450c7036b30003cb288
Herman Bonnes <herman@sentillia.com>
2023-01-09 16:33:01 +0100

    JSON err response for find_mail endpoint

--- src/endpoints.rs
+++ src/endpoints.rs
@@ -1,5 +1,6 @@
 use axum::extract::Query;
 use axum::Json;
+use axum::response::ErrorResponse;
 use log::info;
 use rustc_hash::FxHashMap;
 use serde::{Deserialize, Serialize};
@@ -12,7 +13,13 @@ pub struct FindMailQuery {
 }
 
 #[derive(Serialize)]
-pub struct FindMailResponse(FxHashMap<String, Vec<Mail>>);
+pub struct FindMailResponse {
+    #[serde()]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    results: Option<FxHashMap<String, Vec<Mail>>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    error: Option<String>,
+}
 
 pub async fn find_mail(query: Query<FindMailQuery>) -> axum::response::Result<Json<FindMailResponse>> {
     let mdb = MAIL_DB.lock();
@@ -36,11 +43,16 @@ pub async fn find_mail(query: Query<FindMailQuery>) -> axum::response::Result<Js
         })
         .filter(|(_, v)| !v.is_empty())
         .collect();
+
     if !mail_db_results.is_empty() {
-        Ok(Json(FindMailResponse(mail_db_results)))
+        Ok(Json(FindMailResponse {
+            results: Some(mail_db_results),
+            error: None,
+        }))
     } else {
-        Err(axum::response::ErrorResponse::from(
-            format!("No mails found for query: {} with filter {}\n", &query.email_address_filter, subject_filter)
-        ))
+        Ok(Json(FindMailResponse {
+            results: None,
+            error: Some(format!("No mails found for query '{}' with subject filter '{}'", &query.email_address_filter, subject_filter)),
+        }))
     }
 }
